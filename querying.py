99797787c58f9f4ff652fd0238971e4b23e67a2d
Yoshitaka Moriwaki <virgospica93@gmail.com>
2019-10-06 15:29:05 +0900

    add show_bumps

--- querying.py
+++ querying.py
@@ -644,6 +644,31 @@ DESCRIPTION
     return [get_coords(selection, state)
             for state in range(1, cmd.count_states(selection) + 1)]
 
+
+def show_bumps(selection='(all)', name='bump_check', quiet=1):
+    '''
+DESCRIPTION
+
+    Visualize VDW clashes
+
+ARGUMENTS
+
+    selection = string: atom selection {default: all}
+
+    name = string: name of CGO object to create {default: bump_check}
+    '''
+    cmd.delete(name)
+    cmd.create(name, selection, zoom=0)
+    cmd.set('sculpt_vdw_vis_mode', 1, name)
+    cmd.set('sculpt_field_mask', 0x020)  # cSculptVDW
+    for state in range(1, 1 + cmd.count_states('%' + name)):
+        cmd.sculpt_activate(name, state)
+        strain = cmd.sculpt_iterate(name, state, cycles=0)
+        if not int(quiet):
+            print('VDW Strain in state %d: %f' % (state, strain))
+    cmd.show_as('cgo', name)
+
+
 if 'centerofmass' not in cmd.keyword:
     cmd.extend('centerofmass', centerofmass)
 cmd.extend('gyradius', gyradius)
@@ -652,6 +677,7 @@ cmd.extend('get_sasa_ball', get_sasa_ball)
 cmd.extend('get_sasa_mmtk', get_sasa_mmtk)
 cmd.extend('get_raw_distances', get_raw_distances)
 cmd.extend('print_dihedrals', print_dihedrals)
+cmd.extend('show_bumps', show_bumps)
 
 cmd.auto_arg[0].update([
     ('centerofmass', cmd.auto_arg[0]['zoom']),
